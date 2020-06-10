907c4a7d6d62bac869d76249dd5498f77f22833b
cas380 <57329598+cas380@users.noreply.github.com>
2020-06-10 19:08:01 -0400

    Adding a second page

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 # Right now the login is the root page
 # We might want to allow users access to the site without logging in
 @app.route('/')
-@app.route('/login.html')
+def main():
+	return render_template("index.html", page_title="CrossFilmz")
+
+@app.route("/login.html")
 def login():
-	return render_template('login.html', page_title="CrossFilmz Login")
+	return render_template("login.html", page_title="CrossFilmz Login")
 
 # For local testing
-if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == "__main__":
+	app.run(host="127.0.0.1", port=8080, debug=True)
