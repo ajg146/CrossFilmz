bb2ca82973ddf09be9bd72b66f572ca6819ecd14
samirsherlekar <sas450@pitt.edu>
2020-06-12 19:51:32 -0400

    added folder for components

---
+++ crossfilmz-app/src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+
+class Search extends React.Component {
+  render() {
+    
+
+    
+    );
+  }
+}
+
+export default Search;
--- crossfilmz-app/src/index.js
+++ crossfilmz-app/src/index.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/Hello";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, React!</h1>
+        <Hello></Hello>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
