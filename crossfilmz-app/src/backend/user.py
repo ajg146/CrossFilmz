1a67d0e185b332b4dc46d1962d2f8de526edc05c
apeiffer <apeiffer11@gmail.com>
2020-06-20 12:29:06 -0400

    Moved unnecessary database stuff into class

--- crossfilmz-app/src/backend/user.py
+++ crossfilmz-app/src/backend/user.py
@@ -25,52 +25,36 @@ class User:
 
         db_ops.close_db_conn(conn)
 
-    def _update_rating(self, movie, score, cur):
-        sql_command = """
-            UPDATE user_ratings
-            SET rating = ?
-            WHERE login = ?
-            AND title = ?"""
-        cur.execute(sql_command, (score, self.login, str(movie.title)))
-
     def add_rating(self, movie, score):
         conn, cur = db_ops.open_db_conn()
 
         if score < MIN_SCORE or score > MAX_SCORE:
             return # Make this be a more informative exception
 
-        sql_command = """
-            SELECT *
-            FROM user_ratings
-            WHERE title = ?"""
-        cur.execute(sql_command, (str(movie.title),))
-
-        if cur.fetchone() is not None:
-            print('here')
-            self._update_rating(movie, score, cur)
-
+        if movie.title not in self.ratings:
+            for tag in movie.tags:
+                self.vector[tag] = self.vector[tag] + score if tag in self.vector else score
         else:
-            sql_command = """
-                INSERT INTO user_ratings
-                (login, title, rating)
-                VALUES (?, ?, ?)"""
+            previous_rating = self.ratings[movie.title]
+            for tag in movie.tags:
+                self.vector[tag] += (self.vector[tag] - previous_rating)
+        self.ratings[movie.title] = score
 
-            cur.execute(sql_command, (self.login, str(movie.title), score))
-
-        # Check if they already rated the movie - don't increase the count if so
-        # For tag in movie's tags update the user's vector i guess
         db_ops.close_db_conn(conn)
 
     def __init__(self, login):
         self.login = login
         self.vector = {}
+        self.ratings = {}
         self.add_user_to_db()
 
 # Using for testing
 def main():
     user = User('aep67')
-    movie = Movie('testmovie')
+    movie = Movie('testmovie', ['action', 'drama'])
     user.add_rating(movie, 2)
+    movie2 = Movie('other', ['action', 'comedy'])
+    user.add_rating(movie2, 5)
 
 if __name__ == "__main__":
     main()
