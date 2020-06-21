f6c7c70e5fd02cc55393efec8b176a3dee094a39
samirsherlekar <sas450@pitt.edu>
2020-06-21 18:30:06 -0400

    cleaning up ui

--- crossfilmz-app/src/index.js
+++ crossfilmz-app/src/index.js
@@ -1,26 +1,12 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Hello from "./components/Hello";
-import Search from "./components/Search";
-import MovieCard from "./components/MovieCard";
-import Recomend from "./components/Recomend";
-import Rating from "./components/Rating";
 import Drawer from "./components/Drawer";
-import Avatar from "./components/Avatar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Drawer />
-
-        {/* <Search></Search>
-        <div>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-        </div>
-        <Recomend></Recomend> */}
       </div>
     );
   }
