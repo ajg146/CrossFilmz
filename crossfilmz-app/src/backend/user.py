e8a41322fc5551b3fb923a1ff75ce82362d88a7a
apeiffer <apeiffer11@gmail.com>
2020-06-21 14:20:35 -0400

    add-update-ratings (#6)
    * Moved unnecessary database stuff into class
    
    * Removed unnecessary SQL
    
    * Removed unnecessary tables
    
    * Fixed add/update calculation

--- crossfilmz-app/src/backend/user.py
+++ crossfilmz-app/src/backend/user.py
@@ -25,52 +25,44 @@ class User:
 
         db_ops.close_db_conn(conn)
 
-    def _update_rating(self, movie, score, cur):
-        sql_command = """
-            UPDATE user_ratings
-            SET rating = ?
-            WHERE login = ?
-            AND title = ?"""
-        cur.execute(sql_command, (score, self.login, str(movie.title)))
-
     def add_rating(self, movie, score):
-        conn, cur = db_ops.open_db_conn()
-
         if score < MIN_SCORE or score > MAX_SCORE:
             return # Make this be a more informative exception
 
-        sql_command = """
-            SELECT *
-            FROM user_ratings
-            WHERE title = ?"""
-        cur.execute(sql_command, (str(movie.title),))
-
-        if cur.fetchone() is not None:
-            print('here')
-            self._update_rating(movie, score, cur)
-
+        if movie.title not in self.ratings:
+            for tag in movie.tags:
+                self.vector[tag] = (self.vector[tag] *
+                                    self.rating_count + score) / \
+                                    (self.rating_count + 1) \
+                                    if tag in self.vector else score
+            self.rating_count += 1
         else:
-            sql_command = """
-                INSERT INTO user_ratings
-                (login, title, rating)
-                VALUES (?, ?, ?)"""
+            previous_rating = self.ratings[movie.title]
+            for tag in movie.tags:)
+                self.vector[tag] += (score - previous_rating) / \
+                                    self.rating_count
+        self.ratings[movie.title] = score
 
-            cur.execute(sql_command, (self.login, str(movie.title), score))
-
-        # Check if they already rated the movie - don't increase the count if so
-        # For tag in movie's tags update the user's vector i guess
-        db_ops.close_db_conn(conn)
+    def get_ratings(self):
+        return self.ratings
 
     def __init__(self, login):
         self.login = login
         self.vector = {}
+        self.ratings = {}
+        self.rating_count = 0
         self.add_user_to_db()
 
-# Using for testing
-def main():
-    user = User('aep67')
-    movie = Movie('testmovie')
-    user.add_rating(movie, 2)
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     user = User('aep67')
+#     movie = Movie('testmovie', ['action', 'drama'])
+#     user.add_rating(movie, 2)
+#     movie2 = Movie('other', ['action', 'comedy'])
+#     user.add_rating(movie2, 5)
+#     user.add_rating(movie, 3)
+#     print(user.ratings)
+#     print(user.vector)
+#
+# if __name__ == "__main__":
+#     main()
