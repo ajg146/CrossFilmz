53e862f25e4b9d0bfc299c86a5fc48bdd1647b33
apeiffer <apeiffer11@gmail.com>
2020-06-27 06:03:37 -0400

    Get recs (#7)
    * Started working on getting recommendations
    
    * Finally got .gitignore working for __pycache__
    
    * Basic get_recs implemented
    
    * Fixed issue with line feed/carriage return characters so movie can be directly entered into db

--- crossfilmz-app/src/backend/user.py
+++ crossfilmz-app/src/backend/user.py
@@ -2,7 +2,7 @@ import sqlite3
 import db_ops
 from movie import Movie
 
-MIN_SCORE = 1 # maybe put these in their own file so frontend can reference them too
+MIN_SCORE = 1
 MAX_SCORE = 5
 
 
@@ -46,6 +46,37 @@ class User:
     def get_ratings(self):
         return self.ratings
 
+    def get_recs(self):
+        recs = {'netflix': {}, 'hulu': {}, 'amazon': {}, 'disney': {}}
+
+        conn, cur = db_ops.open_db_conn()
+        sql_command = """
+            SELECT * FROM movies"""
+        cur.execute(sql_command)
+        movies = cur.fetchall()
+        db_ops.close_db_conn(conn)
+
+        for movie in movies:
+            movie_score = 0
+            title = movie[0]
+            tags = movie[1].strip('][').split(', ')
+            platforms = movie[2].strip('][').split(', ')
+
+            for tag in tags:
+                tag = tag.strip('\'')
+                if tag in self.vector:
+                    movie_score += self.vector[tag]
+                else:
+                    movie_score += 2.5
+
+            for platform in platforms:
+                # These strips shouldn't cause any issues, but be sure to check
+                # them if any bugs arise
+                platform = platform.strip('\r').strip(']\n').strip('\'')
+                recs[platform][title] = movie_score
+
+        return recs
+
     def __init__(self, login):
         self.login = login
         self.vector = {}
@@ -54,15 +85,16 @@ class User:
         self.add_user_to_db()
 
 
-# def main():
-#     user = User('aep67')
-#     movie = Movie('testmovie', ['action', 'drama'])
-#     user.add_rating(movie, 2)
-#     movie2 = Movie('other', ['action', 'comedy'])
-#     user.add_rating(movie2, 5)
-#     user.add_rating(movie, 3)
-#     print(user.ratings)
-#     print(user.vector)
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    user = User('testuser')
+    movie = Movie('testmovie', ['action', 'drama'], ['netflix', 'hulu'])
+    user.add_rating(movie, 2)
+    movie2 = Movie('other', ['action', 'comedy'], ['disney', 'amazon'])
+    user.add_rating(movie2, 5)
+    user.add_rating(movie, 3)
+    print(user.ratings)
+    print(user.vector)
+    print(user.get_recs())
+
+if __name__ == "__main__":
+    main()
