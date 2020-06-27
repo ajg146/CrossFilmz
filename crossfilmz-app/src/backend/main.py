1082838b68fe11b6bcc6c50cdd9642f65b51cbc2
samirsherlekar <sas450@pitt.edu>
2020-06-27 14:15:57 -0400

    playing with endpoints

--- crossfilmz-app/src/backend/main.py
+++ crossfilmz-app/src/backend/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from movie import Movie
 
 app = Flask(__name__)
 
 # Routing! Whatever app.route() contains is the href of a link, the end of the URL.
 
+
 @app.route('/')
 def main_page():
-	return render_template("index.html", page_title="CrossFilmz")
+    return movie.query.all()
+
+
+@app.route('/test')
+def test():
+    return "test"
+
 
 # For local testing
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
