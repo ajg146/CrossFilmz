2b66c4f0bcbd4c1ef74e87c17a3e8c30a1bda131
samirsherlekar <sas450@pitt.edu>
2020-07-15 14:07:13 -0400

    deprecate componentDidMount on apibutton

--- crossfilmz-app/src/components/ApiButton.js
+++ crossfilmz-app/src/components/ApiButton.js
@@ -5,13 +5,6 @@ import ListItemText from "@material-ui/core/ListItemText";
 import AllInclusiveIcon from "@material-ui/icons/AllInclusive";
 
 class ApiButton extends React.Component {
-  async componentDidMount() {
-    const url = "http://127.0.0.1:5000/get_movies";
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("hi");
-    console.log(data);
-  }
   render() {
     return (
       <ListItem
