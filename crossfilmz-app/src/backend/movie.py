0302c95457baa2da0c25ab3a5b2f474e5a13b1d4
apeiffer <apeiffer11@gmail.com>
2020-07-18 21:51:52 -0400

    Added type check to select_some_movies

--- crossfilmz-app/src/backend/movie.py
+++ crossfilmz-app/src/backend/movie.py
@@ -42,16 +42,18 @@ class Movie:
 
     @staticmethod
     def select_some_movies(platforms):
+        if isinstance(platforms, str):
+            platforms = [platforms]
+
         rows = []
         conn, cur = db_ops.open_db_conn()
-        print(platforms)
+
         for platform in platforms:
-            print(platform)
-            query = "SELECT * FROM movies WHERE availability LIKE '%{}%'".format(
-                platform)
-            print(query)
+            query = "SELECT * FROM movies \
+                     WHERE availability LIKE '%{}%'".format(platform)
             cur.execute(query)
             rows.append(cur.fetchall())
+
         return rows
 
     def __init__(self, title, given_tags=None, available_platforms=None):
