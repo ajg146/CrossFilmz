a422fb5258ad750e42a81012b540d787ab0aa295
samirsherlekar <sas450@pitt.edu>
2020-07-26 18:16:32 -0400

    UI works with /get_rating

--- crossfilmz-app/src/components/Rating.js
+++ crossfilmz-app/src/components/Rating.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import Rating from "@material-ui/lab/Rating";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -16,13 +16,13 @@ export default function HalfRating(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   useEffect(() => {
-    async function test(){
-    const url = "http://127.0.0.1:5000/get_rating";
-    const response = await fetch(url);
-    const data = await response.json();
-    setValue(data["rating"]);
+    async function test() {
+      const url = "http://127.0.0.1:5000/get_rating";
+      const response = await fetch(url);
+      const data = await response.json();
+      setValue(data["rating"]);
     }
-    test()
+    test();
   });
   return (
     <div className={classes.root}>
